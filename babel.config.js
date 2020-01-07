099c5964d7d750fd135ca69f07150578acc97fbe
Rocky Feng <folowing@gmail.com>
2020-01-07 14:16:31 +0800

    Fix: plugins

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+module.exports = function(api) {
+  api.cache(true);
+
+  const presets = ['@babel/env', '@babel/typescript', '@babel/react'];
+
+  const plugins = [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-transform-runtime',
+  ];
+
+  return { presets, plugins };
+};
